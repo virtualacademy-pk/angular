75dc2f3cc2be7a889b2aa92028833c486bde153f
Technical Lead <sk8027@outlook.com>
2022-09-17 16:47:38 +0300

    added category component

--- Fundamentals/src/app/app.module.ts
+++ Fundamentals/src/app/app.module.ts
@@ -1,11 +1,12 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
+import {CategoryListComponent} from './products/category-list/category-list.component';
 import { AppComponent } from './app.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    CategoryListComponent
   ],
   imports: [
     BrowserModule
